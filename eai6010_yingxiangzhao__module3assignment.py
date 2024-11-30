9bbe789cbc410485cdc84e79ea0de122e46c0fa8
JeasonZhao0421 <zhaojeason@yahoo.com>
2024-11-30 14:34:57 -0800

    Update eai6010_yingxiangzhao__module3assignment.py

--- eai6010_yingxiangzhao__module3assignment.py
+++ eai6010_yingxiangzhao__module3assignment.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
-from fastai.text.all import *
 import matplotlib.pyplot as plt
 
 # Load train and test data with specified encoding
